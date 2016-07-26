15a65b5f6ab96e196adfe45b9fec956b7911b28b
Simic Marko <simic.marko91m@gmail.com>
2016-07-26 14:16:55 +0200

    Zavrsen prvi slajder i pokrenut je drugi slajder i tabulator

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+$(document).ready(function () {
 
+    //Podesavanje funkcije za brzinu automatske promene slike
+    $('.carousel').carousel({
+        interval: 6000 //changes the speed
+    });
+
+    //Funkcija za tabulator
+    $('#myTabs a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
+
+    $('#myTabs a[href="#profile"]').tab('show'); // Select tab by name
+    $('#myTabs a:first').tab('show'); // Select first tab
+    $('#myTabs a:last').tab('show'); // Select last tab
+    $('#myTabs li:eq(2) a').tab('show'); // Select third tab (0-indexed)
+
+
+
+
+
+
+
+});
