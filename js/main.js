d379ac672055c8c534322cf87e0f140c58893a87
Jovan Radmilovic <jovanbg89@gmail.com>
2016-07-27 05:19:01 +0200

    Zavrsena sekcije sa slajderom i sekcija sa slajderom sa cetiri slike

--- js/main.js
+++ js/main.js
@@ -17,10 +17,50 @@ $(document).ready(function () {
     $('#myTabs a:last').tab('show'); // Select last tab
     $('#myTabs li:eq(2) a').tab('show'); // Select third tab (0-indexed)
 
+    //Funkcija za Slider sa 4 slike
+    
 
 
 
-
-
-
+});
+$(document).ready(function () {
+    $('.userComment').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
