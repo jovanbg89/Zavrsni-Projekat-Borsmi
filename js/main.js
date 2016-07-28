11ee74e8b0a6ef378ea126d8184a37d9b638c36e
Jovan Radmilovic <jovanbg89@gmail.com>
2016-07-28 03:02:55 +0200

    Dodat jos jedan slajder i jedna sekcija.

--- js/main.js
+++ js/main.js
@@ -44,18 +44,14 @@ $(document).ready(function () {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2,
-                    infinite: true,
-                    dots: true
+                    slidesToScroll: 2
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
+                    slidesToScroll: 1
                 }
             }
             // You can unslick at a given breakpoint now by adding:
