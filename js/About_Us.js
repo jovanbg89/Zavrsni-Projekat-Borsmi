3070ded423f3303e3525cb9dc93846d221bae127
Jovan Radmilovic <jovanbg89@gmail.com>
2016-08-02 02:01:35 +0200

    Zavrsena About Us stranica i zapoceta Menu stranica.

---
+++ js/About_Us.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    //Podesavanje funkcije za brzinu automatske promene slike
+    $('.carousel').carousel({
+        interval: 6000 //changes the speed
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
