273bf94c7f8f9491efd47d6ad15837c5e97cc11b
Jovan Radmilovic <jovanbg89@gmail.com>
2016-08-08 05:21:05 +0200

    Zavrseno sve osim stranice Contact.

--- js/main.js
+++ js/main.js
@@ -57,7 +57,153 @@ $(document).ready(function () {
         ]
     });
 
+
 });
 
 //Funkcija za datepicker
+$('.form_date').datetimepicker({
+    weekStart: 1,
+    todayBtn: 1,
+    autoclose: 1,
+    todayHighlight: 1,
+    startView: 2,
+    minView: 2,
+    forceParse: 0
+});
+$('.form_time').datetimepicker({
+    language: 'fr',
+    weekStart: 1,
+    todayBtn: 1,
+    autoclose: 1,
+    todayHighlight: 1,
+    startView: 1,
+    minView: 0,
+    maxView: 1,
+    forceParse: 0
+});
+(function (b, o, i, l, e, r) {
+    b.GoogleAnalyticsObject = l;
+    b[l] || (b[l] =
+            function () {
+                (b[l].q = b[l].q || []).push(arguments)
+            });
+    b[l].l = +new Date;
+    e = o.createElement(i);
+    r = o.getElementsByTagName(i)[0];
+    e.src = '//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e, r)
+}(window, document, 'script', 'ga'));
+ga('create', 'UA-XXXXX-X', 'auto');
+ga('send', 'pageview');
 
+
+
+
+
+//Rate star
+$(function () {
+
+    var rating = 1.6;
+
+    $(".counter").text(rating);
+
+    $("#rateYo1").on("rateyo.init", function () {
+        console.log("rateyo.init");
+    });
+
+    $("#rateYo1").rateYo({
+        rating: rating,
+        numStars: 7,
+        precision: 2,
+        starWidth: "64px",
+        spacing: "5px",
+        multiColor: {
+            startColor: "#000000",
+            endColor: "#000000"
+        },
+        onInit: function () {
+
+            console.log("On Init");
+        },
+        onSet: function () {
+
+            console.log("On Set");
+        }
+    }).on("rateyo.set", function () {
+        console.log("rateyo.set");
+    })
+            .on("rateyo.change", function () {
+                console.log("rateyo.change");
+            });
+
+    $(".rateyo").rateYo();
+
+    $(".rateyo-readonly-widg").rateYo({
+        rating: rating,
+        numStars: 5,
+        precision: 2,
+        minValue: 1,
+        maxValue: 5
+    }).on("rateyo.change", function (e, data) {
+
+        console.log(data.rating);
+    });
+
+
+
+
+    //Form select
+    $('.selectpicker').selectpicker('val', 'Mustard');
+});
+
+
+
+//Product gallery
+$(document).ready(function () {
+    $('a').click(function () {
+        var largeImage = $(this).attr('data-full');
+        $('.selected').removeClass();
+        $(this).addClass('selected');
+        $('.full img').hide();
+        $('.full img').attr('src', largeImage);
+        $('.full img').fadeIn();
+
+
+    }); // closing the listening on a click
+    $('.full img').on('click', function () {
+        var modalImage = $(this).attr('src');
+        $.fancybox.open(modalImage);
+    });
+}); //closing our doc ready
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+
+$(document).ready(function () {
+    //Povecavanje i smanjivanje
+    $('.down').click(function () {
+        var txt = $('.number').val();
+        if (txt <= 1) {
+            $('.number').val(1);
+        } else {
+            txt--;
+            $('.number').val(txt);
+        }
+    });
+    $('.up').click(function () {
+        var txt = $('.number').val();
+        txt++;
+        $('.number').val(txt);
+    });
+});
